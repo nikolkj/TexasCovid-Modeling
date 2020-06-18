d37961415c80132db3637e6102fda9907f0b4c22
Nikolai Priser <niko@pop-os.localdomain>
2020-06-18 17:16:35 -0500

    county-level tab item development

--- TexasCovid-Models/server.R
+++ TexasCovid-Models/server.R
@@ -7,94 +7,344 @@
 #    http://shiny.rstudio.com/
 #
 
-# Load Required Data
-dat_ts = readRDS(file = "/home/niko/Documents/R-projects/TexasCovid-modeling/dat_ts.RDS")
+# LOAD EXTERNAL PLOTS ----
+plot_TexasCommunities.hist = readRDS(file = "../plot_Texas-Communities_Population-Segmentation.RDS")
+plot_TexasCommunities.map = readRDS(file = "../plot_Texas-Communities_Population-Segmentation-Map.RDS")
 
-# Prepare Data-Objects -----
-# County-level Data
-dat = dat_ts %>% 
-    select(County, data) %>% 
-    unnest(data = ., cols = c(data))
-
-# State-level Data
-dat_state = dat %>% 
-    select(-County) %>% 
-    group_by(Date) %>% 
-    summarise_at(vars(-group_cols()), sum)
-
-# Define Generic Plotting Parameters ----
+# DEF. GENERIC PLOTTING PARAMETERS ----
+# >>FONTS<<
 plotly_titlefont.axis = list(family = "Courier New, monospace",
                              size = 18,
                              color = "#7f7f7f")
+# >>AXIS FORMATTING<<
+plotly_axisformat.date = list(
+    titlefont = plotly_titlefont.axis,
+    type = "date",
+    tickformat = "%m/%d",
+    tickangle = -15
+)
+
+# >>PLOTTING COLORS<<
+# @90% opacity, unless noted otherwise
+plotly_color.cases = "#ffe77c" # @95% opacity
+plotly_color.tests = "#5beb90" 
+plotly_color.deaths = "#ff4837"
 
 # Define Server-side Operations ----
 shinyServer(function(input, output) {
     
-    
-    output$plot.state.new_cases.bar = renderPlotly({
+    # PLOTS: "state.daily_*" ----
+    output$plot.state.daily_cases.bar = renderPlotly({
         p = dat_state %>%
             plot_ly(
                 data = .,
                 x = ~ Date,
                 y = ~ DailyDelta_cases,
+                marker = list(color = plotly_color.cases),
+                opacity = .95,
                 type = "bar"
             ) %>%
             layout(
                 p = .,
-                xaxis = list(
-                    title = "Date", titlefont = plotly_titlefont.axis,
-                    type = "date",
-                    tickformat = "%m/%d"
-                ),
+                xaxis = plotly_axisformat.date,
                 yaxis = list(title = "Daily Cases", titlefont = plotly_titlefont.axis)
             )
         
         p
     })
     
-    output$plot.state.new_tests.bar = renderPlotly({
+    output$plot.state.daily_tests.bar = renderPlotly({
         p = dat_state %>%
             plot_ly(
                 data = .,
                 x = ~ Date,
                 y = ~ DailyDelta_tests,
+                marker = list(color = plotly_color.tests),
+                opacity = .90,
                 type = "bar"
             ) %>%
             layout(
                 p = .,
-                xaxis = list(
-                    title = "Date", titlefont = plotly_titlefont.axis,
-                    type = "date",
-                    tickformat = "%m/%d"
-                ),
+                xaxis = plotly_axisformat.date,
                 yaxis = list(title = "Daily Tests", titlefont = plotly_titlefont.axis)
             )
         
         p
     })
     
-    output$plot.state.new_deaths.bar = renderPlotly({
+    output$plot.state.daily_deaths.bar = renderPlotly({
         p = dat_state %>%
             plot_ly(
                 data = .,
                 x = ~ Date,
-                y = ~ DailyDelta_cases,
+                y = ~ DailyDelta_deaths,
+                marker = list(color = plotly_color.deaths),
+                opacity = .90,
                 type = "bar"
             ) %>%
             layout(
                 p = .,
-                xaxis = list(
-                    title = "Date", titlefont = plotly_titlefont.axis,
-                    type = "date",
-                    tickformat = "%m/%d"
-                ),
+                xaxis = plotly_axisformat.date,
                 yaxis = list(title = "Daily Deaths", titlefont = plotly_titlefont.axis)
             )
         
         p
     })
     
+    output$plot.state.daily_cases.curveicon = renderPlot({
+        temp
+    })
+    
+    # PLOTS: "state.total_*" ----
+    output$plot.state.total_cases.line = renderPlotly({
+        p = dat_state %>%
+            plot_ly(
+                data = .,
+                x = ~ Date,
+                y = ~ DailyCount_cases,
+                line = list(color = plotly_color.cases, width = 8),
+                opacity = .95,
+                type = 'scatter', mode = 'lines'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Total Cases", titlefont = plotly_titlefont.axis, type = "log")
+            )
+        
+        p
+    })
+    
+    output$plot.state.total_tests.line = renderPlotly({
+        p = dat_state %>%
+            plot_ly(
+                data = .,
+                x = ~ Date,
+                y = ~ DailyCount_tests,
+                line = list(color = plotly_color.tests, width = 8),
+                opacity = .90,
+                type = 'scatter', mode = 'lines'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Total Tests", titlefont = plotly_titlefont.axis, type = "log")
+            )
+        
+        p
+    })
+    
+    output$plot.state.total_deaths.line = renderPlotly({
+        p = dat_state %>%
+            plot_ly(
+                data = .,
+                x = ~ Date,
+                y = ~ DailyCount_deaths,
+                line = list(color = plotly_color.deaths, width = 8),
+                opacity = .90,
+                type = 'scatter', mode = 'lines'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Total Deaths", titlefont = plotly_titlefont.axis, type = "log")
+            )
+        
+        p
+    })
+    
+    # PLOTS: "state.rate_*" ----
+    # Rates should use numerator data as COLOR and OPACITY reference
+    output$plot.state.rate_detection.line = renderPlotly({
+        p = dat_state %>%
+            mutate(daily_detection = round((DailyDelta_cases/DailyDelta_tests), 2)) %>%
+            filter(!is.na(daily_detection)) %>%
+            plot_ly(
+                data = .,
+                x = ~ Date,
+                y = ~ daily_detection,
+                line = list(color = plotly_color.cases, width = 5),
+                opacity = .95,
+                type = 'scatter', mode = 'lines'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Detection Rate", titlefont = plotly_titlefont.axis, tickformat = ".2%")
+            )
+        
+        p
+        
+        
+    })
+    
+    output$plot.state.rate_mortality.line = renderPlotly({
+        p = dat_state %>%
+            mutate(daily_mortality = (DailyDelta_deaths/DailyDelta_cases)) %>%
+            filter(!is.na(daily_mortality)) %>%
+            plot_ly(
+                data = .,
+                x = ~ Date,
+                y = ~ daily_mortality,
+                line = list(color = plotly_color.deaths, width = 5),
+                opacity = .90,
+                type = 'scatter', mode = 'lines'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Mortality Rate", titlefont = plotly_titlefont.axis, tickformat = ".2%")
+            )
+        
+        p
+        
+        
+    })
+    
+    # PLOTS: "comm.rates_*"
+    # Rates should use numerator data as COLOR and OPACITY reference
+    output$comm.rates_infected.line = renderPlotly({
+        dat_pop %>%
+            group_by(Date, pop_group) %>%
+            mutate(pcnt_infected = DailyCount_cases/Population) %>% 
+            filter(!is.na(pcnt_infected)) %>%
+            select(Date, pop_group, pcnt_infected) %>%
+            ungroup() %>%
+            plot_ly(data = ., 
+                    x = ~Date,
+                    y = ~pcnt_infected,
+                    color = ~pop_group,
+                    mode = 'lines', 
+                    line = list(width = 3),
+                    # opacity = .90,
+                    type = 'scatter'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Pcnt Population Infected", titlefont = plotly_titlefont.axis, tickformat = ".2%")
+            )
+    })
+    
+    output$comm.rates_dailyinfected.line = renderPlotly({
+        dat_pop %>%
+            group_by(Date, pop_group) %>%
+            mutate(pcnt_infected = DailyDelta_cases/Population) %>% 
+            filter(!is.na(pcnt_infected)) %>%
+            select(Date, pop_group, pcnt_infected) %>%
+            ungroup() %>%
+            plot_ly(data = ., 
+                    x = ~Date,
+                    y = ~pcnt_infected,
+                    color = ~pop_group,
+                    mode = 'lines', 
+                    line = list(width = 3),
+                    # opacity = .90,
+                    type = 'scatter'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Daily Infection Rate", titlefont = plotly_titlefont.axis, tickformat = ".2%")
+            )
+    })
     
     
+    output$comm.rates_tested.line = renderPlotly({
+        dat_pop %>%
+            group_by(Date, pop_group) %>%
+            mutate(pcnt_tested = DailyCount_tests/Population) %>% 
+            filter(!is.na(pcnt_tested)) %>%
+            select(Date, pop_group, pcnt_tested) %>%
+            ungroup() %>%
+            plot_ly(data = ., 
+                    x = ~Date,
+                    y = ~pcnt_tested,
+                    color = ~pop_group,
+                    mode = 'lines', 
+                    line = list(width = 3),
+                    # opacity = .90,
+                    type = 'scatter'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Pcnt Population Tested", titlefont = plotly_titlefont.axis, tickformat = ".2%")
+            )
+        
+    })
+    
+    output$comm.rates_dailytested.line = renderPlotly({
+        dat_pop %>%
+            group_by(Date, pop_group) %>%
+            mutate(pcnt_tested = DailyDelta_tests/Population) %>% 
+            filter(!is.na(pcnt_tested)) %>%
+            select(Date, pop_group, pcnt_tested) %>%
+            ungroup() %>%
+            plot_ly(data = ., 
+                    x = ~Date,
+                    y = ~pcnt_tested,
+                    color = ~pop_group,
+                    mode = 'lines', 
+                    line = list(width = 3),
+                    # opacity = .90,
+                    type = 'scatter'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Daily Testing Rate", titlefont = plotly_titlefont.axis, tickformat = ".2%")
+            )
+    })
+    
+    output$comm.rates_mortality.line = renderPlotly({
+        dat_pop %>%
+            group_by(Date, pop_group) %>%
+            mutate(pcnt_mortality = DailyCount_deaths/DailyCount_cases) %>% 
+            filter(!is.na(pcnt_mortality)) %>%
+            select(Date, pop_group, pcnt_mortality) %>%
+            ungroup() %>%
+            plot_ly(data = ., 
+                    x = ~Date,
+                    y = ~pcnt_mortality,
+                    color = ~pop_group,
+                    mode = 'lines', 
+                    line = list(width = 3),
+                    # opacity = .90,
+                    type = 'scatter'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Case Mortality Rate", titlefont = plotly_titlefont.axis, tickformat = ".2%")
+            )
+    })
+    
+    output$comm.rates_dailymortality.line = renderPlotly({
+        dat_pop %>%
+            group_by(Date, pop_group) %>%
+            mutate(pcnt_mortality = DailyDelta_deaths/DailyDelta_cases) %>% 
+            filter(!is.na(pcnt_mortality)) %>%
+            select(Date, pop_group, pcnt_mortality) %>%
+            ungroup() %>%
+            plot_ly(data = ., 
+                    x = ~Date,
+                    y = ~pcnt_mortality,
+                    color = ~pop_group,
+                    mode = 'lines', 
+                    line = list(width = 3),
+                    # opacity = .90,
+                    type = 'scatter'
+            ) %>%
+            layout(
+                p = .,
+                xaxis = plotly_axisformat.date,
+                yaxis = list(title = "Daily Case Mortality Rate", titlefont = plotly_titlefont.axis, tickformat = ".2%")
+            )
+    })
+    
+    # PLOTS: "comm.info_*" ----
+    output$comm.info_segementation.hist = renderPlot({plot_TexasCommunities.hist})
+    output$comm.info_segementation.map = renderPlot({plot_TexasCommunities.map})
     
 })
--- TexasCovid-Models/ui.R
+++ TexasCovid-Models/ui.R
@@ -7,6 +7,8 @@
 #    http://shiny.rstudio.com/
 #
 
+rm(list = ls());
+
 # Load general packages 
 require(tidyverse)
 
@@ -16,6 +18,49 @@ require(shinydashboard)
 require(dashboardthemes) # experimental, install_github("nik01010/dashboardthemes")
 require(plotly, quietly = TRUE)
 
+# LOAD REQUIRED DATA ---
+dat_ts = readRDS(file = "../dat_ts.RDS") # Master data set
+pop = readRDS(file = "../texas-demographics_county-populations_segmented.RDS")
+
+# PREPARE DATA-OBJECTS -----
+# County-level Data
+dat <<- dat_ts %>% 
+    select(County, data) %>% 
+    unnest(data = ., cols = c(data))
+
+# State-level Data
+dat_state <<- dat %>% 
+    select(-County) %>% 
+    group_by(Date) %>% 
+    summarise_at(vars(-group_cols()), sum)
+
+# Community-level Data
+dat_pop <<- dat %>% 
+    left_join(x = .,
+              y = (pop %>% 
+                       select(County, pop_group, jan1_2019_pop_est) %>%
+                       rename(Population = jan1_2019_pop_est)
+                   ), 
+              by = "County") %>%
+    select(-County) %>% 
+    group_by(pop_group, Date) %>% 
+    summarise_at(vars(-group_cols()), sum)
+        
+
+# CALCULATE DYNAMIC METRICS ----
+# ... Data loaded in "server.r" 
+
+# >>STATE TAB<<
+state.comps.new_cases = (sum(dat_state$DailyDelta_cases < dat_state$DailyDelta_cases[nrow(dat_state)]) / nrow(dat_state) * 100) %>%
+    round(x = ., digits = 0)
+
+state.comps.new_tests = (sum(dat_state$DailyDelta_tests > dat_state$DailyDelta_tests[nrow(dat_state)]) / nrow(dat_state) * 100) %>%
+    round(x = ., digits = 0)
+
+state.comps.new_deaths = (sum(dat_state$DailyDelta_deaths < dat_state$DailyDelta_deaths[nrow(dat_state)]) / nrow(dat_state) * 100) %>%
+    round(x = ., digits = 0)
+
+# DEFINE UI ELEMENTS ----
 # Define sidebar contents
 ui_sidebar = dashboardSidebar(
     sidebarMenu(
@@ -37,17 +82,93 @@ ui_body = dashboardBody(
     dashboardthemes::shinyDashboardThemes(theme = "grey_dark"), # set theme from package::dashboardthemes
     tabItems(
         tabItem(tabName = "tab_dash_state", 
-                h2("Daily Cases"),
-                plotly::plotlyOutput(outputId = "plot.state.new_cases.bar", width = "50%"),
+                fluidRow(
+                    tabBox(title = tagList(shiny::icon(name = "stethoscope", class = "fa-1x",lib = "font-awesome"), 
+                                           HTML("<b>Tests</b>")), selected = "Daily",
+                           tabPanel("Total",
+                                    plotly::plotlyOutput(outputId = "plot.state.total_tests.line")),
+                           tabPanel("Daily",
+                                    plotly::plotlyOutput(outputId = "plot.state.daily_tests.bar"),
+                                    HTML(paste0("<br>Appx. <b>", state.comps.new_tests, "%</b> of the days were better.</br>"))),
+                           tabPanel("Forecasts", 
+                                    "... Coming Soon ...")
+                           
+                    ),
+                    
+                    tabBox(title = tagList(shiny::icon(name = "ambulance", class = "fa-1x",lib = "font-awesome"), 
+                                           HTML("<b>Cases</b>")), selected = "Daily",
+                           tabPanel("Total",
+                                    plotly::plotlyOutput(outputId = "plot.state.total_cases.line")),
+                           tabPanel("Daily",
+                                    plotly::plotlyOutput(outputId = "plot.state.daily_cases.bar"),
+                                    HTML(paste0("<br>Appx. <b>", state.comps.new_cases, "%</b> of the days were better.</br>"))
+                           ),
+                           tabPanel("Forecasts", 
+                                    "... Coming Soon ...")
+                           
+                    )
+                    
+                ),
                 
-                h2("Daily Tests"),
-                plotly::plotlyOutput(outputId = "plot.state.new_tests.bar", width = "50%"),
-                
-                h2("Daily Deaths"),
-                plotly::plotlyOutput(outputId = "plot.state.new_deaths.bar", width = "50%"),
+                fluidRow(
+                    tabBox(title = tagList(shiny::icon(name = "skull", class = "fa-1x",lib = "font-awesome"), 
+                                           HTML("<b>Deaths</b>")), selected = "Daily",
+                           tabPanel("Total",
+                                    plotly::plotlyOutput(outputId = "plot.state.total_deaths.line")),
+                           tabPanel("Daily",
+                                    plotly::plotlyOutput(outputId = "plot.state.daily_deaths.bar"),
+                                    HTML(paste0("<br>Appx. <b>", state.comps.new_deaths, "%</b> of the days were better.</br>"))),
+                           tabPanel("Forecasts", 
+                                    "... Coming Soon ...")
+                           
+                    ),
+                    tabBox(title = tagList(shiny::icon(name = "compass", class = "fa-1x",lib = "font-awesome"), 
+                                           HTML("<b>Rates</b>")), selected = "Detection Rate",
+                           tabPanel("Detection Rate",
+                                    plotly::plotlyOutput(outputId = "plot.state.rate_detection.line")),
+                           tabPanel("Mortality Rate",
+                                    plotly::plotlyOutput(outputId = "plot.state.rate_mortality.line")),
+                           tabPanel("Notes", 
+                                    "... Coming Soon ...")
+                           
+                    )
+                    
+                )
         ),
-        tabItem(tabName = "tab_dash_community", "community"),
-        tabItem(tabName = "tab_dash_county", "county"),
+        tabItem(tabName = "tab_dash_community", 
+                fluidRow(
+                    tabBox(title = tagList(shiny::icon(name = "compass", class = "fa-1x",lib = "font-awesome"), 
+                                           HTML("<b>Rates</b>")),
+                           tabPanel("Infection Rate",
+                                    plotly::plotlyOutput(outputId = "comm.rates_infected.line")),
+                           tabPanel("Daily Infection Rate",
+                                    plotly::plotlyOutput(outputId = "comm.rates_dailyinfected.line")),
+                           tabPanel("Testing Rate",
+                                    plotly::plotlyOutput(outputId = "comm.rates_tested.line")),
+                           tabPanel("Daily Testing Rate",
+                                    plotly::plotlyOutput(outputId = "comm.rates_dailytested.line")),
+                           tabPanel("Mortality Rate", 
+                                    plotly::plotlyOutput(outputId = "comm.rates_mortality.line")),
+                           tabPanel("Daily Mortality Rate", 
+                                    plotly::plotlyOutput(outputId = "comm.rates_dailymortality.line"))
+                    ),
+                    tabBox(title = tagList(shiny::icon(name = "info-circle", class = "fa-1x",lib = "font-awesome"), 
+                                           HTML("<b>Segmentation Details</b>")), selected = "Map",
+                           tabPanel("Map", 
+                                    shiny::plotOutput(outputId = "comm.info_segementation.map", width = "700px", height = "600px")),
+                           tabPanel("Curve", 
+                                    shiny::plotOutput(outputId = "comm.info_segementation.hist", width = "700px", height = "600px"))
+                    )
+                )),
+        tabItem(tabName = "tab_dash_county", 
+                shiny::fluidRow(
+                    tabBox(title = tagList(shiny::icon(name = "compass", class = "fa-1x",lib = "font-awesome"), 
+                                           HTML("<b>Rates</b>")),
+                           tabPanel("Infection Rate",
+                                    "asdf")
+                    )
+                )
+                ),
         tabItem(tabName = "tab_other_about", "about"),
         tabItem(tabName = "tab_other_support", "support")
     )
